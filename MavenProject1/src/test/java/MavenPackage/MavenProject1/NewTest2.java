2e682647020739470788643f56e7ab4d42b80065
parth235 <pawal.pankaj@gmail.com>
2018-07-07 15:45:05 +0530

    Commit 5

--- MavenProject1/src/test/java/MavenPackage/MavenProject1/NewTest2.java
+++ MavenProject1/src/test/java/MavenPackage/MavenProject1/NewTest2.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class NewTest2 {
   @Test
   public void f() {
-	  System.out.println("----------jmp shr1");
+	  System.out.println("----------jmp shr1235");
 
   }
   
