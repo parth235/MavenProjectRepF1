d63f06f3f7bad5232951711df67819e442d7b964
parth235 <pawal.pankaj@gmail.com>
2018-07-07 15:26:59 +0530

    Second Test NG

---
+++ MavenProject1/src/test/java/MavenPackage/MavenProject1/NewTest2.java
@@ -0,0 +1,12 @@
+package MavenPackage.MavenProject1;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  System.out.println("----------jmp");
+
+  }
+  
+}
